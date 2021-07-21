f026900fdaef185fe83dbbf23493cbdf08adcdab
Abner Pena <abnrr.pena@gmail.com>
2021-07-21 16:00:04 -0400

    New Components

---
+++ PREC/src/app/calculator/Calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Calculator',
+  templateUrl: './Calculator.component.html',
+  styleUrls: ['./Calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
