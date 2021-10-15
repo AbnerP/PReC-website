86f3c23b22d7a6001599dd365a1b581eb9e2dada
Abner Pena <abnrr.pena@gmail.com>
2021-10-15 18:43:20 -0400

    .

--- PREC-API/PREC-API/Models/Tire.cs
+++ PREC-API/PREC-API/Models/Tire.cs
@@ -1,29 +1,29 @@
-﻿using System;
-namespace PREC_API.Models
-{
-    public enum Tire
-    {
-        SuperSoft,
-        Soft,
-        Medium,
-        Hard,
-        Intermediate,
-        Wet
-    }
-    {
-  "stint":{    
-    "tire": "Soft",
+﻿// using System;
+// namespace PREC_API.Models
+// {
+//     public enum Tire
+//     {
+//         SuperSoft,
+//         Soft,
+//         Medium,
+//         Hard,
+//         Intermediate,
+//         Wet
+//     }
+//     {
+//   "stint":{    
+//     "tire": "Soft",
     
-    "startingLap":"0",
-    "inLap": "2",
+//     "startingLap":"0",
+//     "inLap": "2",
     
-    "totalTime":"3:51.2",
-    "length":"2",
-    "pitStop":{
-      "tire":"Hard",
-      "fuel": "23"
-    }
-  }
-}
+//     "totalTime":"3:51.2",
+//     "length":"2",
+//     "pitStop":{
+//       "tire":"Hard",
+//       "fuel": "23"
+//     }
+//   }
+// }
 
-}
+// }
