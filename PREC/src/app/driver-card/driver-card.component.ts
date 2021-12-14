095c6af641151c6b61f8d98f5e005caf3886a132
Abner Pena <abnrr.pena@gmail.com>
2021-12-14 17:26:52 -0500

    Resolved #27 & #28

--- PREC/src/app/driver-card/driver-card.component.ts
+++ PREC/src/app/driver-card/driver-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-driver-card',
@@ -7,28 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriverCardComponent implements OnInit {
 
-  imageURL:string;
-  name:string;
-  consoles:Array<string>;
-  games:Array<string>;
-  gamertag:string;
-  kudosPrimeLink:string;
-  teamRole:Array<string>;
-  nativeTown:string;
-  racingMemory:string;
+  @Input() imageURL:string;
+  @Input() name:string;
+  // consoles:Array<string>;
+  // games:Array<string>;
+  @Input() gamertag:string;
+  @Input() kudosPrimeLink:string;
+  @Input() teamRole:Array<string>;
+  // nativeTown:string;
+  // racingMemory:string;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.imageURL = "assets/imgs/Abner_Pena_Portrait.jpg";
-    this.name="Abner Peña";
-    this.consoles=["PC","PS4"];
-    this.games=["ACC","GT SPORT"];
-    this.gamertag="PReC_APena";
-    this.teamRole=["Driver","Web Page Maintance"];
-    this.kudosPrimeLink="https://www.kudosprime.com/gts/stats.php?profile=10733830";
-    this.nativeTown="Caguas, PR";
-    this.racingMemory = "Watching Hamilton wint the 2008 F1 title";
   }
 
 }
