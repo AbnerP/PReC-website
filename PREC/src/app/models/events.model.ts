4d0dfe8821cd0290143a0addf1cfc0f49ce6e270
Abner Pena <abnrr.pena@gmail.com>
2021-12-21 14:24:04 -0400

    2. eventDTO model created #50

---
+++ PREC/src/app/models/events.model.ts
@@ -0,0 +1,16 @@
+export interface eventsDTO{
+  
+}
+
+export interface eventDTO{
+  _id:string;
+  name: string;
+  date:string;
+  startTime: string;
+  game:string;
+  track:string;
+  duration:string;
+  description: string;
+  imageURL:string;
+  contactInfo:string;
+}
--- PREC/src/app/services/events.service.ts
+++ PREC/src/app/services/events.service.ts
@@ -1,9 +1,54 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import axios from "axios";
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  private apiURL = environment.backendAPIURL + '/events';
+
+  // async getDrivers(){
+  //   const res = await axios.get<driversDTO>(this.apiURL);
+  //   for(let driver of res.data.drivers){
+  //     driver.imageURL = environment.backendAPIURL + '/'+ driver.imageURL;
+  //   }
+  //   return res.data;
+  // }
+
+  // async getDriverByID(id:string){
+  //   const res = await axios.get<driversDTO>(this.apiURL+`/${id}`);
+  //   return res.data;
+  // }
+
+  // async updateDriverImage(id:string,img:string){
+  //   const res = await axios.patch(this.apiURL+`/${id}`,{imageURL:img});
+  //   console.log(res.data);
+  //   return res.data;
+  // }
+
+  // async deleteDriver(id:string){
+  //   const res = await axios.delete(this.apiURL+`/${id}`);
+  //   console.log(res.data);
+  //   return res.data;
+  // }
+
+  // async createDriver(driver:driverInfo,img:File){
+  //   const fd = new FormData();
+  //   if(img != null){
+  //     fd.append('driverImage',img,img.name);
+  //   }
+  //   fd.append('name',driver.name);
+  //   fd.append('gamertag',driver.gamertag);
+  //   fd.append('kudosPrimeLink',driver.kudosPrimeLink);
+  //   for(let role of driver.teamRole){
+  //     fd.append('teamRole',role);
+  //   }
+  //   return await axios.post(this.apiURL,fd);
+  // }
 }
