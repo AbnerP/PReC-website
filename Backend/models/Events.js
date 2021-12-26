a8ecdf48a9ccd6dc2008eae416ca1f63ef173c6a
Abner Pena <abnrr.pena@gmail.com>
2021-12-25 21:06:52 -0400

    Events schema updated #89

--- Backend/models/Events.js
+++ Backend/models/Events.js
@@ -4,12 +4,17 @@ const eventSchema = mongoose.Schema({
     name: {type:String,required:true},
     date:{type:Date,required:false},
     startTime:{type:String,required:true},
+    host:{type:String},
+    platform:{type:String,required:true},
     game:{type:String,required:true},
     track:{type:String,required:true},
     duration: {type:String,required:true},
     description: {type:String,required:true},
     imageURL: {type:String,required:true},
-    contactInfo:{type:String,required:true}
+    contactInfo:{type:String,required:true},
+    registrationLimit:{type:Number,required:true},
+    numberRegisteredUsers:{type:Number, default:0},
+    registeredUserIDsList:{type:[String],default:[]},
 });
 
 module.exports = mongoose.model('Events',eventSchema);
