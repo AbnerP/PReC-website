f3d15cdfc86d8f5274dc22b0c07cd17ff4d0709a
Abner Pena <abnrr.pena@gmail.com>
2021-12-30 19:02:37 -0400

    .

--- PREC/src/environments/environment.prod.ts
+++ PREC/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  googleapiURL: 'https://www.googleapis.com/calendar/v3',
+  calculatorApiURL: 'https://prec-api.azurewebsites.net/api',
+  backendAPIURL: 'https://puertoricoeracingclub.herokuapp.com/api'
+
 };
